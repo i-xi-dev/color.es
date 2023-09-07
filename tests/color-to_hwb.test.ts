8d565e779e86df228d6eb8495d62c98ef875b86a
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-08 00:32:41 +0900

    2.0.0

--- tests/color-to_hwb.test.ts
+++ tests/color-to_hwb.test.ts
@@ -1,41 +1,41 @@
 import { assertStrictEquals } from "./deps.ts";
-import { SRgbColor } from "../mod.ts";
+import { Color } from "../mod.ts";
 import { r1Cases, r2Cases, r3Cases } from "./data.ts";
 
-Deno.test("SRgbColor.prototype.toHwb()", () => {
+Deno.test("Color.prototype.toHwb()", () => {
   for (const c of r1Cases) {
-    const hsl = SRgbColor.fromRgb({ r: c.r, g: 0, b: 0 }).toHwb();
+    const hsl = Color.fromRgb({ r: c.r, g: 0, b: 0 }).toHwb();
     assertStrictEquals(hsl.h.toFixed(2), c.h);
     assertStrictEquals(hsl.w.toFixed(4), c.wt);
     assertStrictEquals(hsl.b.toFixed(4), c.bk);
   }
 
   for (const c of r2Cases) {
-    const hsl = SRgbColor.fromRgb({ r: 255, g: c.x, b: c.x }).toHwb();
+    const hsl = Color.fromRgb({ r: 255, g: c.x, b: c.x }).toHwb();
     assertStrictEquals(hsl.h.toFixed(2), c.h);
     assertStrictEquals(hsl.w.toFixed(4), c.wt);
     assertStrictEquals(hsl.b.toFixed(4), c.bk);
   }
 
   for (const c of r3Cases) {
-    const hsl = SRgbColor.fromRgb({ r: c.r, g: c.g, b: c.b }).toHwb();
+    const hsl = Color.fromRgb({ r: c.r, g: c.g, b: c.b }).toHwb();
     assertStrictEquals(hsl.h.toFixed(2), c.h);
     assertStrictEquals(hsl.w.toFixed(4), c.wt);
     assertStrictEquals(hsl.b.toFixed(4), c.bk);
   }
 
-  const c0 = SRgbColor.fromHexString("#fffefd");
+  const c0 = Color.fromHexString("#fffefd");
   const c1 = c0.toHwb();
   c1.h = 120;
   assertStrictEquals(c0.red, 1);
 });
 
-Deno.test("SRgbColor.prototype.toHwb({}) - discardAlpha", () => {
-  const c1 = SRgbColor.fromHexString("#fffefd");
+Deno.test("Color.prototype.toHwb({}) - discardAlpha", () => {
+  const c1 = Color.fromHexString("#fffefd");
   const c1a = c1.toHwb({ discardAlpha: true });
   assertStrictEquals("a" in c1a, false);
 
-  const c2 = SRgbColor.fromHexString("#fffefd88");
+  const c2 = Color.fromHexString("#fffefd88");
   const c2a = c2.toHwb({ discardAlpha: true });
   assertStrictEquals("a" in c2a, false);
 });
