30f79440f6eafc19e68fc1ceb3e2cc29d56adb79
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-28 21:47:05 +0900

    5.0.1

--- src/rgb/rgb.ts
+++ src/rgb/rgb.ts
@@ -26,41 +26,67 @@ type Rgb = {
 };
 
 namespace Rgb {
+  /**
+   * A component (r, g, or b) of RGB color.
+   */
   export type Component = number;
 
   export namespace Component {
+    /**
+     * The minimum value of component.
+     */
     export const MIN_VALUE = 0;
 
+    /**
+     * The maximum value of component.
+     */
     export const MAX_VALUE = 1;
 
-    //XXX 現バージョンでは、範囲は0-1で固定（sRGB以外は考慮しない）
-    export function normalize(value: unknown): Component {
-      if (Number.isFinite(value)) {
-        return NumberUtils.clamp(value as number, MIN_VALUE, MAX_VALUE);
+    //XXX 現バージョンでは、0以上1以下に正規化する（＝sRGB以外は考慮しない）
+    /**
+     * Normalize a component value to the range of 0 to 1.
+     *
+     * In the current version, normalized values are restricted to be greater than or equal to 0 and less than or equal to 1.
+     *
+     * @param component - A component value
+     * @returns A component value normalized to the range of 0 to 1.
+     */
+    export function normalize(component: unknown): Component {
+      if (Number.isFinite(component)) {
+        return NumberUtils.clamp(component as number, MIN_VALUE, MAX_VALUE);
       }
       return MIN_VALUE;
     }
   }
 
+  /**
+   * A normalized RGB components.
+   */
   export type Normalized = {
     r: Component;
     g: Component;
     b: Component;
   };
 
-  export function normalize(value: Rgb): Normalized {
+  /**
+   * Normalize RGB components.
+   *
+   * @param components - RGB components.
+   * @returns RGB components where the value of each component is normalized from 0 to 1.
+   */
+  export function normalize(components: Rgb): Normalized {
     let r: unknown = undefined;
     let g: unknown = undefined;
     let b: unknown = undefined;
-    if (value && (typeof value === "object")) {
-      if ("r" in value) {
-        r = value.r;
+    if (components && (typeof components === "object")) {
+      if ("r" in components) {
+        r = components.r;
       }
-      if ("g" in value) {
-        g = value.g;
+      if ("g" in components) {
+        g = components.g;
       }
-      if ("b" in value) {
-        b = value.b;
+      if ("b" in components) {
+        b = components.b;
       }
     }
     return {
