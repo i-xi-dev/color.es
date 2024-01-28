b41b33bc7ebcf9716af21c0144b1a10a6030bcb0
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:23:31 +0900

    5.2.8

--- build_npm.ts
+++ build_npm.ts
@@ -15,7 +15,7 @@ await build({
   rootTestDir: "./tests",
   package: {
     name: "@i-xi-dev/color",
-    version: "5.2.7",
+    version: "5.2.8",
     description: "A JavaScript immutable object that represents the color.",
     license: "MIT",
     author: "i-xi-dev",
--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export {
   NumberEx,
   RoundingMode,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
-export { Angle } from "https://raw.githubusercontent.com/i-xi-dev/angle.es/1.2.10/mod.ts";
-export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.27/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.2/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
+export { Angle } from "https://raw.githubusercontent.com/i-xi-dev/angle.es/1.2.11/mod.ts";
+export { BytesFormat } from "https://raw.githubusercontent.com/i-xi-dev/bytes-format.es/1.0.28/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
